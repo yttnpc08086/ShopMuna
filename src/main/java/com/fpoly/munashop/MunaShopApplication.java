3db236e3be95eba5522b232c0f7f7d559b4ce51c
yttnpc08086 <166134557+yttnpc08086@users.noreply.github.com>
2024-09-22 00:12:12 +0700

    buil src

---
+++ src/main/java/com/fpoly/munashop/MunaShopApplication.java
@@ -0,0 +1,13 @@
+package com.fpoly.munashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MunaShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fpoly/munashop/MunaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpoly.munashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunaShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
